442d82309bfca8c3e63b4cc549544dedfb01a238
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-10-05 14:34:00 +0600

    Update API URL to production

--- script.js
+++ script.js
@@ -409,7 +409,7 @@ async function showFinalResult() {
 
     try {
         // Call FastAPI backend
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://consumer-rights.onrender.com/predict", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload)
