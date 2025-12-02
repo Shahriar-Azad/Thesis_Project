738f2a2e566a19b185584617e30321cadda77553
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-12-03 02:49:15 +0600

    svm prediction fixed

--- predict.py
+++ predict.py
@@ -4,11 +4,10 @@ from fastapi.middleware.cors import CORSMiddleware
 import pickle
 import os
 import numpy as np
-# from mangum import Mangum
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, "./model.pkl")
-ENCODER_PATH = os.path.join(BASE_DIR, "./encoder.pkl")
+MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
+ENCODER_PATH = os.path.join(BASE_DIR, "encoder.pkl")
 
 # -------------------------------
 # Load Model + Encoder
@@ -20,7 +19,7 @@ with open(ENCODER_PATH, "rb") as f:
     encoder = pickle.load(f)
 
 # -------------------------------
-# Input Encoding (simple)
+# Input Encoding
 # -------------------------------
 def encode_inputs(inputs: list):
     encoded = np.array([encoder.transform([i])[0] for i in inputs])
@@ -32,18 +31,20 @@ def encode_inputs(inputs: list):
 def predict_top3_laws(inputs: list):
     encoded = encode_inputs(inputs)
     proba = model.predict_proba(encoded)[0]
+
     top3_idx = np.argsort(proba)[-3:][::-1]
     top3_laws = model.classes_[top3_idx]
 
     return {
-        "predicted_law": top3_laws[0],
-        "suggested_laws": list(top3_laws[1:])
+        "predicted_law": int(top3_laws[0]),
+        "suggested_laws": [int(l) for l in top3_laws[1:]]
     }
 
 # -------------------------------
-# FastAPI App
+# FastAPI Setup
 # -------------------------------
 app = FastAPI(title="Legal Assessment API")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -68,9 +69,8 @@ def predict_endpoint(data: Answers):
         "properLabeling", "counterfeit", "harmfulSubstances"
     ]
 
+    # Default missing answers to "No"
     input_values = [answers_dict.get(k, "No") for k in question_keys_ordered]
+
     result = predict_top3_laws(input_values)
-
     return result
-
-# handler = Mangum(app)
